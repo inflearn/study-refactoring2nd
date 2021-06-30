521bab9e7f151f2c34ed717144931b2bc0d665ae
PAPICO <ocipap0531@gmail.com>
2021-07-01 01:01:43 +0900

    new: amountFor function

--- inflearn/ch1/index.js
+++ inflearn/ch1/index.js
@@ -1,6 +1,7 @@
 import { invoices, plays } from './0rigin/data';
 import { statementOrigin } from './0rigin/statement';
 import { statementHoori } from './hoori/statement.js';
+import { statementPapico } from './papico';
 
 const ch_i = 1;
 let i = 1;
@@ -15,6 +16,7 @@ for (let invoice of invoices) {
     check(
       statementOrigin(invoice, plays),
       statementHoori(invoice, plays),
+      statementPapico(invoice, plays),
     ),
     new Date);
 }
---
+++ inflearn/ch1/papico/index.js
@@ -0,0 +1,49 @@
+const usd = new Intl.NumberFormat(
+  'en-US',
+  { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format;
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case 'tragedy': // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy': // 희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount;
+}
+
+export const statementPapico = (invoice, plays) => {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+    // 포인트 적립
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극은 관객 5명마다 추가 포인트
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역 출력
+    result += `  ${play.name}: ${usd(thisAmount / 100)} (${perf.audience}석)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${usd(totalAmount/100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+};
