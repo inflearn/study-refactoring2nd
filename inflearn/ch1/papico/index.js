6dfee55372b2e1d85425ddee1405808acc66f42c
PAPICO <ocipap0531@gmail.com>
2021-07-01 02:24:03 +0900

    new: 다형성 활용하기

--- inflearn/ch1/papico/index.js
+++ inflearn/ch1/papico/index.js
@@ -1,75 +1,23 @@
-function renderPlainText(invoice, plays) {
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+import createStatementData from './createStatementData';
 
-  for (let perf of invoice.performances) {
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+function renderPlainText(data) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience}석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount() / 100)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount / 100)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat(
-      'en-US',
-      { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber);
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
 }
 
-
 export const statementPapico = (invoice, plays) => {
-  return renderPlainText(invoice, plays);
+  return renderPlainText(createStatementData(invoice, plays), plays);
 };
+
+function usd(aNumber) {
+  return new Intl.NumberFormat(
+    'en-US',
+    { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber);
+}
