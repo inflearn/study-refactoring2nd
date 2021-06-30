5adc0e3326904ab602d25e946d2f6826335fd573
PAPICO <ocipap0531@gmail.com>
2021-07-01 01:19:28 +0900

    new: volumeCreditsFor function

--- inflearn/ch1/papico/index.js
+++ inflearn/ch1/papico/index.js
@@ -3,9 +3,6 @@ const usd = new Intl.NumberFormat(
   { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format;
 
 export const statementPapico = (invoice, plays) => {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
@@ -35,12 +32,21 @@ export const statementPapico = (invoice, plays) => {
     return result;
   }
 
-  for (let perf of invoice.performances) {
-    // 포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극은 관객 5명마다 추가 포인트
-    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
 
+    return result;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
     // 청구 내역 출력
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
